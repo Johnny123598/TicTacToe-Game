6f980277e3735a88c5e10eef2773db9adb67ded1
Jonathan Romo <jromo5@asu.edu>
2025-11-11 22:16:25 -0700

    Add initial project structure and core classes
    - GameEngine with board initialization
    
    - Config, docs, and test stub

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -8,17 +8,10 @@ public class GameEngine {
 
     private void initializeBoard() {
         for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) board[i][j] = '-';
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
         }
     }
 }
-public boolean makeMove(int row, int col) {
-        if (row < 0 || row >= 3 || col < 0 || col >= 3) return false;
-        if (board[row][col] != '-') return false;
-        board[row][col] = currentPlayer;
-        return true;
-    }
 
-    public void switchPlayer() {
-        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-    }
