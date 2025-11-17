958540765b4ed2ba103188d82722ef4da314a2f1
Jonathan Romo <jromo5@asu.edu>
2025-11-16 20:19:39 -0700

    Add TODO for colored game pieces

---
+++ src/TicTacToe.java
@@ -0,0 +1,9 @@
+public class TicTacToe {
+  public static void main(String[] args){
+    System.out.println("Welcome to TicTacToe!");
+      //TODO: Implement game logic
+   }
+}
+// TODO: add input handling
+// TODO: add winner chicken dinner detection
+// TODO:Add colored output for X and O
